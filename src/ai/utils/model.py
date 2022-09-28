41550086ef30892cec7c7a7d8f207368133cbff4
jayhack <jayhack.0@gmail.com>
2022-09-28 16:58:17 -0700

    .

--- src/ai/utils/model.py
+++ src/ai/utils/model.py
@@ -8,16 +8,22 @@ from .custom_types import ModelQuery
 class Model(APIInterface):
     id: int
     name: str
-    agent_name: str
+    id: int
     base_url: str
 
-    def __init__(self, id: int, name: str, agent_name: str):
+    def __init__(self, id: int, name: str, agent_id: int):
         self.id = id
         self.name = name
-        self.agent_name = agent_name
-        self.base_url = f"{config['server_url']}/agents/{self.agent_name}/models/{self.id}"
+        self.agent_id = agent_id
+        self.base_url = f"{config['server_url']}/agents/models"
 
-    def query(self, raw_query: Any):
+    def query(self, raw_query: Any, use_active_prompt=False, prompt_id=None) -> dict:
         query = ModelQuery(raw_query)
-        response = self._post('/query', query.to_dict())
+        d = query.to_dict()
+        d['model_id'] = self.id
+        d['agent_id'] = self.agent_id
+        d['use_active_prompt'] = use_active_prompt
+        if prompt_id:
+            d['prompt_id'] = prompt_id
+        response = self._post('/query', d)
         return response['payload']['result']  # This is way too long
