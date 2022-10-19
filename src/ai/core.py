ad63bbfa5c18426d5ba3b756a4c46f245f490dc4
jayhack <jayhack.0@gmail.com>
2022-10-19 15:28:58 -0700

    .

--- src/ai/core.py
+++ src/ai/core.py
@@ -52,7 +52,7 @@ app = FastAPI()
 router = APIRouter()
 
 default_channels: List[str] = ['Slack', 'Twitter']
-default_models: List[str] = ['openai/gpt-3', 'stability-ai/stable-diffusion', 'play.ht']
+default_models: List[str] = ['openai/gpt-3', 'stability-ai/stable-diffusion', 'play.ht', 'assembly.ai']
 
 
 class AI(APIInterface):
